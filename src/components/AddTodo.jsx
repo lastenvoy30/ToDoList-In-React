b8ae97faea60c6024a5270bd95ac5e4567fca14e
Mirza Isfand <mirzaisfand321@gmail.com>
2026-01-30 16:57:50 +0530

    replaced usestate with useref

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,41 +1,53 @@
-import { useState , useRef } from "react";
+import { useState , useRef, use } from "react";
 import { IoIosAddCircle } from "react-icons/io";
 
 
 function AddTodo({onNewItem}) {
-  const [todoName , setTodoName] = useState("");
-  const [dueDate , setDueDate] = useState("");
-  const noOfUpdates = useRef(0);
+  // const [todoName , setTodoName] = useState("");
+  // const [dueDate , setDueDate] = useState("");
+  // const noOfUpdates = useRef(0);
+  const todoNameElement = useRef();
+  const dueDateElement = useRef();
 
-  const handleNameChange = (event) => {
-    setTodoName(event.target.value);
-    noOfUpdates.current += 1;
+  // const handleNameChange = (event) => {
+  //   setTodoName(event.target.value);
+  //   noOfUpdates.current += 1;
 
-  };
+  // };
 
-  const handleDateChange = (event) => {
-    setDueDate(event.target.value);
-    console.log(`no of updates are ${noOfUpdates.current}`);
-  }
+  // const handleDateChange = (event) => {
+  //   setDueDate(event.target.value);
+  //   console.log(`no of updates are ${noOfUpdates.current}`);
+  // }
 
   const handleAddButtonClicked = (event) => {
     event.preventDefault();
+    // setDueDate("");
+    // setTodoName("");
+    const todoName = todoNameElement.current.value;
+    const dueDate = dueDateElement.current.value;
+    todoNameElement.current.value = "";
+    dueDateElement.current.value = "";
     onNewItem(todoName , dueDate);
-    setDueDate("");
-    setTodoName("");
   }
 
   return (
     <div className="container text-center">
       <form className="row kg-row">
         <div className="col-6">
-          <input type="text" placeholder="Enter Todo Here" value={todoName} onChange={handleNameChange}/>
+          {/* <input type="text" placeholder="Enter Todo Here" value={todoName} onChange={handleNameChange}/> */}
+          <input type="text" ref = {todoNameElement} placeholder="Enter Todo Here"/>
+
         </div>
         <div className="col-4">
-          <input type="date" value={dueDate} onChange={handleDateChange}/>
+          {/* <input type="date" value={dueDate} onChange={handleDateChange}/> */}
+          <input type="date" ref = {dueDateElement}/>
+
         </div>
         <div className="col-2">
-          <button type="button" className="btn" onClick={handleAddButtonClicked}>
+          <button type="submit" className="btn" onClick={handleAddButtonClicked}>
+          {/* <button type="button" className="btn"> */}
+
             <IoIosAddCircle />
           </button>
         </div>
