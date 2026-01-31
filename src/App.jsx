928637bd9a3fa5d47f5bf90acc51ac2c4b7027a7
Mirza Isfand <mirzaisfand321@gmail.com>
2026-01-31 19:35:56 +0530

    tried and used use reducer hook

--- src/App.jsx
+++ src/App.jsx
@@ -3,69 +3,18 @@ import AddTodo from "./components/AddTodo";
 import TodoItems from "./components/TodoItems";
 import WelcomeMessage from "./components/WelcomeMessage";
 import "./App.css";
-import { useState } from "react";
-import { TodoItemsContext } from "./store/todo-items-store";
+import TodoItemsContextProvider from "./store/todo-items-store";
 
 function App() {
-  // const initialTodoItems = [
-  //   {
-  //     name: "Buy Milk",
-  //     dueDate: "4/10/2023",
-  //   },
-  //   {
-  //     name: "Go to College",
-  //     dueDate: "4/10/2023",
-  //   },
-  //   {
-  //     name: "Go to gym",
-  //     dueDate: "right now",
-  //   }
-  // ];
-
-  // const [todoItems , setTodoItems] = useState(initialTodoItems);
-  const [todoItems, setTodoItems] = useState([]);
-
-  // const handleNewItem = (itemName, itemDueDate) => {
-  const addNewItem = (itemName, itemDueDate) => {
-
-    // console.log(`new item added: ${itemName} date: ${itemDueDate}`);
-    // const newTodoItems = [
-    //   ...todoItems ,
-    //   { name : itemName , dueDate : itemDueDate} ,
-    // ];
-    // setTodoItems(newTodoItems)
-    setTodoItems((currValue) => [
-      ...currValue,
-      { name: itemName, dueDate: itemDueDate },
-    ]);
-  };
-
-  // const handleDeleteItem = (todoItemName) => {
-  const deleteItem = (todoItemName) => {
-
-    // console.log(`Item deleted: ${todoItemName}` );
-    const newTodoItems = todoItems.filter((item) => item.name !== todoItemName);
-    setTodoItems(newTodoItems);
-  };
-  
-  // const defualtTodoItems = [{ name: "buy ghee", dueDate: "today" }]
-
   return (
-    <TodoItemsContext.Provider value={{
-      todoItems,
-      addNewItem, 
-      deleteItem,
-    }}>
+    <TodoItemsContextProvider>
       <center className="todo-container">
         <AppName />
-        <AddTodo/>
+        <AddTodo />
         <WelcomeMessage></WelcomeMessage>
-        <TodoItems
-          todoItems={todoItems}
-          onDeleteClick={deleteItem}
-        ></TodoItems>
+        <TodoItems></TodoItems>
       </center>
-    </TodoItemsContext.Provider>
+    </TodoItemsContextProvider>
   );
 }
 
